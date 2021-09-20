919be6adafe0016d0e1f956cb8b9822778bd64de
Vladimir Bliznyukov <vovablink1@gmail.com>
2021-09-20 21:26:40 +0300

    databases update

--- SQL/tables_creation.sql
+++ SQL/tables_creation.sql
@@ -3,9 +3,8 @@ CREATE TABLE song
     song_id serial PRIMARY KEY NOT NULL,
     name character varying COLLATE pg_catalog."default" NOT NULL,
     author character varying COLLATE pg_catalog."default" NOT NULL,
-    descr character varying COLLATE pg_catalog."default",
-    image_link character varying COLLATE pg_catalog."default"
-)
+    descr character varying COLLATE pg_catalog."default"
+);
 
 CREATE TABLE bot_user
 (
@@ -15,11 +14,11 @@ CREATE TABLE bot_user
     gmt integer,
     time_interval_start integer,
     time_interval_end integer
-)
+);
 
 CREATE TABLE feedback
 (
     feedback_id serial PRIMARY KEY NOT NULL,
     author_id integer REFERENCES bot_user(user_id),
     assessor_id integer REFERENCES bot_user(user_id)
-)
+);
