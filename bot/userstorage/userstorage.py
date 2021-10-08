a73260a5103af5d0ff79d1ecb880e3e01a9b2f18
Vladimir Bazilevich <v.bazilevich@innopolis.university>
2021-10-08 19:00:10 +0300

    Documented user storage

--- bot/userstorage/userstorage.py
+++ bot/userstorage/userstorage.py
@@ -1,13 +1,26 @@
-from bot.userstorage.accessingnonexistingusererror import AccessingNonExistingUserError
 import logging
 import psycopg2
 from bot.userstorage import MissingUserDatabaseURLError
 from bot.userstorage import UpdatingNonExistingUserError
+from bot.userstorage.accessingnonexistingusererror import AccessingNonExistingUserError
 from bot.Schedule import Schedule
 
 
 class UserStorage:
+    """
+    This class implements interaction with database that
+    stores information about users.
+
+    Uses PostgreSQL database. Check SQL folder for database
+    scheme as well as provided procedures.
+    """
     def __init__(self, db_url):
+        """
+        :param db_url: URL of database server with cridentials required
+                       to establish the connection
+        :raises:
+            MissingUserDatabaseURLError: no database URL was provided
+        """
         self.conn = None
         self.cursor = None
         if not db_url:
@@ -17,41 +30,72 @@ class UserStorage:
         self.cursor = self.conn.cursor()
 
     def create_user(self, user_id: int):
+        """
+        Creates a new user with given ID and default values for status,
+        level and schedule
+        :param user_id: Telegram UserID
+        """
         self.cursor.callproc('create_user', [user_id, 1, "active", 0, 10, 18, ])
         self.cursor.fetchone()
 
-    def user_exists(self, user_id: int):
+    def user_exists(self, user_id: int) -> bool:
+        """
+        Check if there is a user with specified ID in database
+        :param user_id: Telegram UserID
+        """
         self.cursor.callproc('user_exists', [user_id])
         result = self.cursor.fetchone()[0]
         logging.debug(f"user_exists({user_id}) = {result}")
         return result
 
     def update_user_schedule(self, user_id: int, schedule: Schedule):
+        """
+        Used to set a new schedule for the user
+        :param user_id: Telegram UserID
+        :param schedule: new schedule of the user
+        :raises:
+            UpdatingNonExistingUserError: no user with given ID
+        """
         if self.user_exists(user_id):
             schedule.update(user_id, self.cursor)
         else:
             raise UpdatingNonExistingUserError(user_id)
 
     def get_user_schedule(self, user_id: int) -> Schedule:
+        """
+        :param user_id: Telegram UserID
+        :returns: schedule that user has registered or default one that was
+                  created while registering user
+        :raises:
+            AccessingNonExistingUserError: no user with given id
+        """
         if self.user_exists(user_id):
             self.cursor.callproc('fetch_user_time', [user_id])
             schedule = self.cursor.fetchone()
-            print(schedule)
             timezone, begin, end = schedule
             result = Schedule(timezone, begin, end)
-            print(result)
             return result
-        else:
-            raise AccessingNonExistingUserError(user_id)
+        raise AccessingNonExistingUserError(user_id)
 
-    def get_user_level(self, user_id: int):
+    def get_user_level(self, user_id: int) -> int:
+        """
+        :param user_id: Telegram UserID
+        :returns: the last level user has passed
+        :raises:
+            AccessingNonExistingUserError: no user with given id
+        """
         if self.user_exists(user_id):
             self.cursor.callproc('fetch_user_level', [user_id])
             return self.cursor.fetchone()
-        else:
-            raise UpdatingNonExistingUserError(user_id)
+        raise AccessingNonExistingUserError(user_id)
 
     def update_user_level(self, user_id: int):
+        """
+        Increments user level
+        :param user_id: Telegram UserID
+        :raises:
+            UpdatingNonExistingUserError: no user with given ID
+        """
         if self.user_exists(user_id):
             self.cursor.callproc('update_user_level', [user_id])
             self.cursor.fetchone()
@@ -59,6 +103,12 @@ class UserStorage:
             raise UpdatingNonExistingUserError(user_id)
 
     def reset_user_progress(self, user_id: int):
+        """
+        Returns user to level 1
+        :param user_id: Telegram UserID
+        :raises:
+            UpdatingNonExistingUserError: no user with given ID
+        """
         if self.user_exists(user_id):
             self.cursor.callproc('reset_user_progress', [user_id])
             self.cursor.fetchone()
